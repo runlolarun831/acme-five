6fbff43574361f5a8da4da96f3f5be0f99aab91d
Lourdes Monahan <lourdes.marija.monahan@gmail.com>
2019-08-24 19:19:34 -0700

    the plan

---
+++ thePlan.js
@@ -0,0 +1,24 @@
+/*the page
+Nav
+Acme 5 - over/ under
+2 links
+-under 5 + list length
+-5 and over + list length
+
+product name link
+w/???average price from offerings under name????
+
+click on product name link and list of offerings along w/ company name
+
+the plan
+
+work on Nav bar
+
+work on all products to get name link
+and ?price
+
+create component that holds offering list
+
+
+
+*/
